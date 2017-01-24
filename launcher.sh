29ff5ff441f9e3b65b8d7f583e6bb7e01153cbdd
Nitish Kandula <nitishkandula@Nitishs-MacBook-Pro.local>
2017-01-24 12:16:49 -0800

    inital_commit

---
+++ launcher.sh
@@ -0,0 +1,2 @@
+aws cloudformation create-stack --stack-name intuitDemo --template-body file:///Users/nitishkandula/Documents/aws_scripts/VPC_nat_chef_bootstraping.json \
+--parameters file:///Users/nitishkandula/Documents/aws_scripts/params.json
